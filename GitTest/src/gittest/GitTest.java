53e781b43159eb71d7920954c873a41605b6d029
e21100369 <e21100369@WIN063.auth.local>
2015-01-22 10:04:40 +0100

    Premier commit IHM

---
+++ GitTest/src/gittest/GitTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author e21100369
+ */
+public class GitTest {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
